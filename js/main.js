18eee21ce8c9294cd1c5bf290b7ec435c0a7abe7
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-27 09:01:49 -0500

    feat: :sparkles: Filtracion de algunos datos correspondientes a rockets
    Filtre datos basicos de rockets

--- js/main.js
+++ js/main.js
@@ -1,34 +1,185 @@
-export const getAllCapsules = async ()=>{
-    let res = await fetch("https://api.spacexdata.com/v4/capsules")
-    let data = await res.json();
-    return data;
-}
-console.log(await getAllCapsules())
+// extraer nombres 
 
-export const getAllCompany = async ()=>{
-    let res = await fetch("https://api.spacexdata.com/v4/company")
-    let data = await res.json();
-    return data;
-}
-console.log(await getAllCompany())
+// export const getAllRocketsName = async() => {
+//     let config = {
+//         headers: {"content-type": "application/json"},
+//         method:"POST",
+//         body: JSON.stringify({
+//             "options": {
+//                 "select": {
+//                 "name": 1
+//                 }
+//             }
+//         })
+//     }
+//     let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
+//     let {docs} = await res.json();
+//     let rocketNames = docs.map(doc => doc.name);
+//     return rocketNames;
+// }
 
-export const getAllCores = async ()=>{
-    let res = await fetch("https://api.spacexdata.com/v4/cores")
-    let data = await res.json();
-    return data;
-}
-console.log(await getAllCores())
+// console.log(await(getAllRocketsName()))
 
-export const getAllCrew = async ()=>{
-    let res = await fetch("https://api.spacexdata.com/v4/crew")
-    let data = await res.json();
-    return data;
-}
-console.log(await getAllCrew())
+// const displayRocketNames = async () => {
+//     const rocketList = document.querySelector('#rockets');
+    
+//     try {
+//         const rocketNames = await getAllRocketsName();
+        
+//         rocketNames.forEach(name => {
+//             const li = document.createElement('li');
+//             li.textContent = name;
+//             rocketList.appendChild(li);
+//         });
+//     } catch (error) {
+//         console.error('Error al obtener nombres de cohetes:', error);
+//     }
+// };
 
-export const getAllDragons = async ()=>{
-    let res = await fetch("https://api.spacexdata.com/v4/crew")
-    let data = await res.json();
-    return data;
+// displayRocketNames();
+
+
+// // extraer info 
+
+// export const getAllRocketsInfo = async() => {
+//     let config = {
+//         headers: {"content-type": "application/json"},
+//         method:"POST",
+//         body: JSON.stringify({
+//             "options": {
+//                 "select": {
+//                 "description": 1
+//             }
+//             }
+//         })
+//     }
+//     let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
+//     let {docs} = await res.json();
+//     let rocketInfo = docs.map(doc => doc.description);
+//     return rocketInfo;
+// }
+
+// console.log(await(getAllRocketsInfo()))
+
+// const displayRocketInfo = async () => {
+//     const rocketList = document.querySelector('#info');
+    
+//     try {
+//         const rocketInfo = await getAllRocketsInfo();
+        
+//         rocketInfo.forEach(description => {
+//             const li = document.createElement('li');
+//             li.textContent = description;
+//             rocketList.appendChild(li);
+//         });
+//     } catch (error) {
+//         console.error('Error al obtener nombres de cohetes:', error);
+//     }
+// };
+
+// displayRocketInfo();
+
+// extraer name e info 
+
+export const getAllRocketsNameInfo = async() => {
+    let config = {
+        headers: {"content-type": "application/json"},
+        method:"POST",
+        body: JSON.stringify({
+            "options": {
+                "select": {
+                "name": 1,
+                "description": 1,
+                "first_flight":1,
+                "company":1,
+                "type":1,
+                "cost_per_launch": 1
+            }
+            }
+        })
+    }
+    let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
+    let {docs} = await res.json();
+    let rocketInfo = docs.map(doc => ({
+        name: doc.name,
+        info: doc.description,
+        flight: doc.first_flight,
+        company: doc.company,
+        type: doc.type,
+        cost: doc.cost_per_launch
+    }));
+    return rocketInfo;
 }
-console.log(await getAllCrew())
+console.log(await(getAllRocketsNameInfo()))
+
+const displayRocketNamesAndInfo = async () => {
+    const rocketList = document.getElementById('mixed');
+    try {
+        const rocketData = await getAllRocketsNameInfo();      
+        rocketData.forEach(({ name, info, flight, company, type, cost}) => {
+            //name
+            const rocketLi = document.createElement('h1');
+            rocketLi.textContent = name;
+
+            //titulo1
+            const infor = document.createElement('h4');
+            infor.textContent = "INFORMATION";
+
+            //informacion
+            const infoPara = document.createElement('p');
+            infoPara.textContent = info;
+
+            //titulo2
+            const infor2 = document.createElement('h4');
+            infor2.textContent = "FIRST FLIGHT";
+
+            //first_flight
+            const vuelo = document.createElement('p');
+            vuelo.textContent = flight;
+
+            //titulo3
+            const infor3 = document.createElement('h4');
+            infor3.textContent = "COMPANY";
+
+            //company
+            const compañia = document.createElement('p');
+            compañia.textContent = company;
+
+            //titulo4
+            const infor4 = document.createElement('h4');
+            infor4.textContent = "TYPE";
+
+            //type
+            const tipo = document.createElement('p');
+            tipo.textContent = type;
+
+            //titulo5
+            const infor5 = document.createElement('h4');
+            infor5.textContent = "COST PER LAUNCH";
+
+            //costo
+            const costo = document.createElement('p');
+            costo.textContent = cost;
+            
+            rocketList.appendChild(rocketLi);
+            rocketList.appendChild(infor);
+            rocketList.appendChild(infoPara);
+            rocketList.appendChild(infor2);
+            rocketList.appendChild(vuelo);
+            rocketList.appendChild(infor3);
+            rocketList.appendChild(compañia);
+            rocketList.appendChild(infor4);
+            rocketList.appendChild(tipo);
+            rocketList.appendChild(infor5);
+            rocketList.appendChild(costo);
+            
+        });
+    } catch (error) {
+        console.error('Error al obtener datos de cohetes:', error);
+    }
+};
+displayRocketNamesAndInfo();
+
+
+
+
