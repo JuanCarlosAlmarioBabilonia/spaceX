f20b6e0d26969f04393f029213bc7662bb920be2
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-29 17:48:46 -0500

    feat: :bento: Adicion de gifs a launches
    Agregue gifs a launches para personalizar un poco la estetica

--- js/modulesComponents/pagination.js
+++ js/modulesComponents/pagination.js
@@ -695,6 +695,48 @@ const getIDLaunches = async (e) => {
     divs.append(div3);
     information__table__2.append(divs)
 
+    let sectionImage = document.querySelector("#section__information__1");
+
+    let img3 = document.createElement("img");
+    img3.setAttribute("src", "storage/img/gif/purple.webp"); 
+    img3.classList.add("imagen-paginacion-roadster");
+    img3.style.position = "absolute";
+    img3.style.top = "270px"; 
+    img3.style.left = "675px"; 
+    img3.style.width = "300px"; 
+    img3.style.height = "150px";
+    img3.style.marginTop = "10px";
+    img3.style.marginLeft = "300px";
+    img3.style.borderRadius = "10%";
+    sectionImage.appendChild(img3);
+
+    let img4 = document.createElement("img");
+    img4.setAttribute("src", "storage/img/gif/purple.webp"); 
+    img4.classList.add("imagen-paginacion-roadster");
+    img4.style.position = "absolute";
+    img4.style.top = "270px"; 
+    img4.style.left = "-230px"; 
+    img4.style.width = "300px"; 
+    img4.style.height = "150px";
+    img4.style.marginTop = "10px";
+    img4.style.marginLeft = "300px";
+    img4.style.borderRadius = "10%";
+    sectionImage.appendChild(img4);
+
+    let img5 = document.createElement("img");
+    img5.setAttribute("src", "storage/img/gif/co.webp");
+    img5.classList.add("imagen-paginacion-roadster");
+    img5.style.position = "absolute";
+    img5.style.top = "270px"; 
+    img5.style.left = "-230px"; 
+    img5.style.width = "300px"; 
+    img5.style.height = "150px";
+    img5.style.marginTop = "-200px";
+    img5.style.marginLeft = "757px";
+    img5.style.borderRadius = "10%";
+    sectionImage.appendChild(img5);
+    
+
 };
 
 
