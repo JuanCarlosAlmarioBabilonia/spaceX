ad0ec7f09ef9912247672e2e6d5049b2f3581576
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-30 12:31:19 -0500

    feat: :sparkles: Tablas de dragons terminadas
    Las tablas fueron terminadas con los datos escogidos a aparecer en esta

--- js/modulesComponents/pagination.js
+++ js/modulesComponents/pagination.js
@@ -1703,6 +1703,84 @@ const getIDDragons = async (e) => {
     let dragonsDiameterFPageElement = await dragonsDiameterFPage(dragons.diameter.feet);
     information__2.append(dragonsDiameterFPageElement);
 
+
+    
+    let information__table__1 = document.querySelector("#information__table__1");
+    information__table__1.innerHTML = "";
+    let h3 = document.createElement("h3");
+    h3.textContent = "Dragons data";
+    let hr = document.createElement("hr");
+    information__table__1.append(h3, hr);
+  
+    let divp = document.createElement("div");
+    divp.classList.add("table__container__1");
+  
+    let div1 = document.createElement("div");
+    let span1 = document.createElement("span");
+    span1.textContent = "Type";
+    let strong1 = document.createElement("strong");
+    strong1.textContent = `${dragons.thrusters[0].type}`;
+    div1.append(span1, strong1);
+  
+    let div2 = document.createElement("div");
+    let span2 = document.createElement("span");
+    span2.textContent = "Amount";
+    let strong2 = document.createElement("strong");
+    strong2.textContent = `${dragons.thrusters[0].amount}`;
+    div2.append(span2, strong2);
+
+    let div3 = document.createElement("div");
+    let span3 = document.createElement("span");
+    span3.textContent = "Pods";
+    let strong3 = document.createElement("strong");
+    strong3.textContent = `${dragons.thrusters[0].pods}`;
+    div3.append(span3, strong3);
+
+    let div4 = document.createElement("div");
+    let span4 = document.createElement("span");
+    span4.textContent = "Fuel 1";
+    let strong4 = document.createElement("strong");
+    strong4.textContent = `${dragons.thrusters[0].fuel_1}`;
+    div4.append(span4, strong4);
+
+    let div5 = document.createElement("div");
+    let span5 = document.createElement("span");
+    span5.textContent = "Fuel 2";
+    let strong5 = document.createElement("strong");
+    strong5.textContent = `${dragons.thrusters[0].fuel_2}`;
+    div5.append(span5, strong5)
+    
+    let div6 = document.createElement("div");
+    let span6 = document.createElement("span");
+    span6.textContent = "Isp";
+    let strong6 = document.createElement("strong");
+    strong6.textContent = `${dragons.thrusters[0].isp}`;
+    div6.append(span6, strong6)
+  
+    divp.append(div1, div2, div3, div4, div5, div6);
+    information__table__1.append(divp);
+
+
+    let information__table__2 = document.querySelector("#information__table__2");
+    information__table__2.innerHTML = "";
+    let h4 = document.createElement("h3");
+    h4.textContent = "Dragons details";
+    let hr1 = document.createElement("hr");
+    information__table__2.append(h4, hr1);
+  
+    let divs= document.createElement("div");
+    divs.classList.add("table__container__2");
+  
+    let div8 = document.createElement("div");
+    let span8 = document.createElement("span");
+    span8.textContent = "Description:";
+    let strong8= document.createElement("strong");
+    strong8.textContent = `${dragons.description}`;
+    div8.append(span8, strong8);
+  
+    divs.append(div8);
+    information__table__2.append(divs)
+
 };
 
 export const paginationDragons = async(page=1, limit=3)=>{  
