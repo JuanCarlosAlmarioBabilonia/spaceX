9e666e69926c625eb23ad0f3f269799edf3192f5
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-29 20:54:30 -0500

    feat: :bento: Adicion de imagenes por defecto y gifs a ships
    Agregue gifs para mejorar la estetica de ships, y las imagenes por defecto

--- js/modulesComponents/card.js
+++ js/modulesComponents/card.js
@@ -74,3 +74,24 @@ export const ImageLandpads = async (landpadsData) => {
         section__image.append(div);
     });
 };
+
+export const imageShips = async (ship) => {
+    console.log(ship);
+    let section__image = document.querySelector("#section__image");
+    section__image.innerHTML = "";
+
+    let div = document.createElement("div");
+    div.classList.add("carousel__item");
+
+    let imageUrl = ship.image || "storage/img/icons/ships.gif"; 
+
+    let img = document.createElement("img");
+    img.setAttribute("src", imageUrl);
+    img.setAttribute("alt", ship.name); 
+    img.setAttribute("referrerpolicy", "no-referrer");
+
+    img.setAttribute("style", "width: 280px; height: 300px; margin-left: 75px; margin-top: -90px");
+
+    div.append(img);
+    section__image.append(div);
+};
--- js/modulesComponents/pagination.js
+++ js/modulesComponents/pagination.js
@@ -144,7 +144,8 @@ import {
     imageRockets,
     imageCrew,
     imageLaunches,
-    ImageLandpads
+    ImageLandpads,
+    imageShips
 } from "./card.js";
 
 import { 
@@ -1025,8 +1026,6 @@ const getIDLandpads = async (e) => {
     let LandpadsWikiPageElement = await landpadsWikiPage(landpads.wikipedia);
     information__2.append(LandpadsWikiPageElement);
 
-    let sectionImage = document.querySelector("#section__information__1");
-
     let information__table__1 = document.querySelector("#information__table__1");
     information__table__1.innerHTML = "";
     let h3 = document.createElement("h3");
@@ -1088,6 +1087,8 @@ const getIDLandpads = async (e) => {
   
     divs.append(div5);
     information__table__2.append(divs)
+
+    let sectionImage = document.querySelector("#section__information__1");
 
     let img3 = document.createElement("img");
     img3.setAttribute("src", "storage/img/gif/purple.webp"); 
@@ -1187,6 +1188,7 @@ const getIDShips = async (e) => {
     console.log(ship);
 
     await nameShips(ship.name);
+    await imageShips(ship);
 
     let description__item = document.querySelector("#description__item");
     description__item.innerHTML = "";
@@ -1293,6 +1295,47 @@ const getIDShips = async (e) => {
   
     divs.append(div8);
     information__table__2.append(divs)
+
+    let sectionImage = document.querySelector("#section__information__1");
+
+    let img3 = document.createElement("img");
+    img3.setAttribute("src", "storage/img/gif/purple.webp"); 
+    img3.classList.add("imagen-paginacion-roadster");
+    img3.style.position = "absolute";
+    img3.style.top = "270px"; 
+    img3.style.left = "675px"; 
+    img3.style.width = "300px"; 
+    img3.style.height = "150px";
+    img3.style.marginTop = "10px";
+    img3.style.marginLeft = "300px";
+    img3.style.borderRadius = "10%";
+    sectionImage.appendChild(img3);
+
+    let img4 = document.createElement("img");
+    img4.setAttribute("src", "storage/img/gif/purple.webp"); 
+    img4.classList.add("imagen-paginacion-roadster");
+    img4.style.position = "absolute";
+    img4.style.top = "270px"; 
+    img4.style.left = "-230px"; 
+    img4.style.width = "300px"; 
+    img4.style.height = "150px";
+    img4.style.marginTop = "10px";
+    img4.style.marginLeft = "300px";
+    img4.style.borderRadius = "10%";
+    sectionImage.appendChild(img4);
+
+    let img5 = document.createElement("img");
+    img5.setAttribute("src", "storage/img/gif/co.webp");
+    img5.classList.add("imagen-paginacion-roadster");
+    img5.style.position = "absolute";
+    img5.style.top = "270px"; 
+    img5.style.left = "-230px"; 
+    img5.style.width = "300px"; 
+    img5.style.height = "150px";
+    img5.style.marginTop = "-200px";
+    img5.style.marginLeft = "757px";
+    img5.style.borderRadius = "10%";
+    sectionImage.appendChild(img5);
 
 };
 
