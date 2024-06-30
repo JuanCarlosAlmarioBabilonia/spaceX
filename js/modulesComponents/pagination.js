91b8ca84e551c793fb6262e8a8a1508e3fc9ec70
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-30 15:32:13 -0500

    feat: :bento: Adicion de gifs y una imagen a payloads
    Agregue gifs y una imagen para mejorar la estetica de payloads

--- js/modulesComponents/pagination.js
+++ js/modulesComponents/pagination.js
@@ -2309,6 +2309,61 @@ const getIDPayloads = async (e) => {
 
     let payloadsInclinationDegPageElement = await payloadsInclinationDegPage(payloads.inclination_deg);
     information__2.append(payloadsInclinationDegPageElement)
+    
+
+    let sectionImage = document.querySelector("#section__information__1");
+
+    let img3 = document.createElement("img");
+    img3.setAttribute("src", "storage/img/gif/purple.webp"); 
+    img3.classList.add("imagen-paginacion-roadster");
+    img3.style.position = "absolute";
+    img3.style.top = "270px"; 
+    img3.style.left = "580px"; 
+    img3.style.width = "300px"; 
+    img3.style.height = "150px";
+    img3.style.marginTop = "85px";
+    img3.style.marginLeft = "300px";
+    img3.style.borderRadius = "10%";
+    sectionImage.appendChild(img3);
+
+    let img4 = document.createElement("img");
+    img4.setAttribute("src", "storage/img/gif/purple.webp");
+    img4.classList.add("imagen-paginacion-roadster");
+    img4.style.position = "absolute";
+    img4.style.top = "270px"; 
+    img4.style.left = "-125px"; 
+    img4.style.width = "300px"; 
+    img4.style.height = "150px";
+    img4.style.marginTop = "85px";
+    img4.style.marginLeft = "300px";
+    img4.style.borderRadius = "10%";
+    sectionImage.appendChild(img4);
+
+    let img5 = document.createElement("img");
+    img5.setAttribute("src", "storage/img/gif/co.webp");
+    img5.classList.add("imagen-paginacion-roadster");
+    img5.style.position = "absolute";
+    img5.style.top = "270px"; 
+    img5.style.left = "-230px"; 
+    img5.style.width = "300px"; 
+    img5.style.height = "150px";
+    img5.style.marginTop = "-200px";
+    img5.style.marginLeft = "757px";
+    img5.style.borderRadius = "10%";
+    sectionImage.appendChild(img5);
+
+    let img6 = document.createElement("img");
+    img6.setAttribute("src", "storage/img/gif/nave.jpeg");
+    img6.classList.add("imagen-paginacion-roadster");
+    img6.style.position = "absolute";
+    img6.style.top = "500px"; 
+    img6.style.left = "-230px"; 
+    img6.style.width = "300px"; 
+    img6.style.height = "300px";
+    img6.style.marginTop = "-200px";
+    img6.style.marginLeft = "757px";
+    img6.style.borderRadius = "10%";
+    sectionImage.appendChild(img6);
 
 };
 
