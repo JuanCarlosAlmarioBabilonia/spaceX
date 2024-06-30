b6aef050d2aff169d26184f242feab2719380f61
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-29 20:07:41 -0500

    feat: :sparkles: Tablas de landpads terminadas
    Las tablas fueron terminadas con los datos escogidos a aparecer en esta

--- js/modulesComponents/pagination.js
+++ js/modulesComponents/pagination.js
@@ -1022,6 +1022,68 @@ const getIDLandpads = async (e) => {
 
     let sectionImage = document.querySelector("#section__information__1");
 
+    let information__table__1 = document.querySelector("#information__table__1");
+    information__table__1.innerHTML = "";
+    let h3 = document.createElement("h3");
+    h3.textContent = "Landpads data";
+    let hr = document.createElement("hr");
+    information__table__1.append(h3, hr);
+  
+    let divp = document.createElement("div");
+    divp.classList.add("table__container__1");
+  
+    let div1 = document.createElement("div");
+    let span1 = document.createElement("span");
+    span1.textContent = "Latitude";
+    let strong1 = document.createElement("strong");
+    strong1.textContent = `${landpads.latitude}`;
+    div1.append(span1, strong1);
+  
+    let div2 = document.createElement("div");
+    let span2 = document.createElement("span");
+    span2.textContent = "Longitude";
+    let strong2 = document.createElement("strong");
+    strong2.textContent = `${landpads.longitude}`;
+    div2.append(span2, strong2);
+
+    let div3 = document.createElement("div");
+    let span3 = document.createElement("span");
+    span3.textContent = "Landing attempts";
+    let strong3 = document.createElement("strong");
+    strong3.textContent = `${landpads.landing_attempts}`;
+    div3.append(span3, strong3);
+
+    let div4 = document.createElement("div");
+    let span4 = document.createElement("span");
+    span4.textContent = "Landing success";
+    let strong4 = document.createElement("strong");
+    strong4.textContent = `${landpads.landing_successes}`;
+    div4.append(span4, strong4);
+  
+    divp.append(div1, div2, div3, div4);
+    information__table__1.append(divp);
+
+
+    let information__table__2 = document.querySelector("#information__table__2");
+    information__table__2.innerHTML = "";
+    let h4 = document.createElement("h3");
+    h4.textContent = "Landpads details";
+    let hr1 = document.createElement("hr");
+    information__table__2.append(h4, hr1);
+  
+    let divs= document.createElement("div");
+    divs.classList.add("table__container__2");
+  
+    let div5 = document.createElement("div");
+    let span5 = document.createElement("span");
+    span5.textContent = "Details:";
+    let strong5= document.createElement("strong");
+    strong5.textContent = `${landpads.details}`;
+    div5.append(span5, strong5);
+  
+    divs.append(div5);
+    information__table__2.append(divs)
+
     let img3 = document.createElement("img");
     img3.setAttribute("src", "storage/img/gif/purple.webp"); 
     img3.classList.add("imagen-paginacion-roadster");
